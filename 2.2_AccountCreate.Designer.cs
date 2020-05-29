9aca475d91925026c48ccf44ffff1f2072bad70f
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-05-29 20:52:59 +0800

    2.2 completed

--- 2.2_AccountCreate.Designer.cs
+++ 2.2_AccountCreate.Designer.cs
@@ -29,8 +29,18 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnBack = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.txtUserID = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtReEnterPassword = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,6 +56,16 @@
             this.panel1.Size = new System.Drawing.Size(933, 68);
             this.panel1.TabIndex = 1;
             // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(12, 21);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(87, 31);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -58,21 +78,106 @@
             this.label1.Text = "ASEAN Skills 2020\r\n";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // btnBack
+            // label2
             // 
-            this.btnBack.Location = new System.Drawing.Point(12, 21);
-            this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(87, 31);
-            this.btnBack.TabIndex = 1;
-            this.btnBack.Text = "Back";
-            this.btnBack.UseVisualStyleBackColor = true;
-            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(290, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(307, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "New Sponsor Account Creation";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(180, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Company Name: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(162, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "User ID (For Login): ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(218, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Password: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(164, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Re-Enter Password: ";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(356, 320);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(153, 37);
+            this.btnCreate.TabIndex = 7;
+            this.btnCreate.Text = "Create Account";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(294, 142);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(303, 21);
+            this.txtCompanyName.TabIndex = 8;
+            // 
+            // txtUserID
+            // 
+            this.txtUserID.Location = new System.Drawing.Point(294, 180);
+            this.txtUserID.Name = "txtUserID";
+            this.txtUserID.Size = new System.Drawing.Size(303, 21);
+            this.txtUserID.TabIndex = 9;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(294, 218);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(303, 21);
+            this.txtPassword.TabIndex = 10;
+            // 
+            // txtReEnterPassword
+            // 
+            this.txtReEnterPassword.Location = new System.Drawing.Point(294, 256);
+            this.txtReEnterPassword.Name = "txtReEnterPassword";
+            this.txtReEnterPassword.Size = new System.Drawing.Size(303, 21);
+            this.txtReEnterPassword.TabIndex = 11;
             // 
             // AccountCreate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.ClientSize = new System.Drawing.Size(933, 423);
+            this.Controls.Add(this.txtReEnterPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserID);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "AccountCreate";
@@ -80,6 +185,7 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -88,5 +194,15 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.TextBox txtUserID;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtReEnterPassword;
     }
 }
--- 2.2_AccountCreate.cs
+++ 2.2_AccountCreate.cs
@@ -23,5 +23,48 @@ namespace Session2_TPRedo
             (new Login()).ShowDialog();
             Close();
         }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (txtCompanyName.Text.Trim() == string.Empty || txtUserID.Text.Trim() == string.Empty || txtPassword.Text.Trim() == string.Empty || txtReEnterPassword.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("One of more fields are empty!");
+            }
+            else if (txtPassword.TextLength < 8)
+            {
+                MessageBox.Show("Password must be 8 characters or more!");
+            }
+            else if (txtPassword.Text != txtReEnterPassword.Text)
+            {
+                MessageBox.Show("Passwords do not match!");
+            }
+            else
+            {
+                using (var context = new Session2Entities())
+                {
+                    var checkUser = (from x in context.Users
+                                     where x.name == txtCompanyName.Text
+                                     select x).FirstOrDefault();
+
+                    var checkUserID = (from x in context.Users
+                                       where x.userId == txtUserID.Text
+                                       select x).FirstOrDefault();
+
+                    if (checkUser != null)
+                    {
+                        MessageBox.Show("Company sponsor account already exist!");
+                    }
+                    else if (checkUserID != null)
+                    {
+                        MessageBox.Show("User ID has been taken!");
+                    }
+                    else
+                    {
+                        context.Users.Add(new User() { name = txtCompanyName.Text, userId = txtUserID.Text, passwd = txtReEnterPassword.Text, userTypeIdFK = 2 });
+                        context.SaveChanges();
+                    }
+                }
+            }
+        }
     }
 }
