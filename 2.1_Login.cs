d7a77f2f6259b4ff1dff81f33667568fd967c69e
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-05-29 20:36:43 +0800

    Add project files.

---
+++ 2.1_Login.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Session2_TPRedo
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            (new AccountCreate()).ShowDialog();
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUserID.Text.Trim() == string.Empty || txtPassword.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("One or more fields are empty!");
+            }
+            else
+            {
+                using (var context = new Session2Entities())
+                {
+                    var user = (from x in context.Users
+                                where x.userId == txtUserID.Text
+                                select x).FirstOrDefault();
+
+                    if (user == null)
+                    {
+                        MessageBox.Show("User does not exit!");
+                    }
+                    else if (user.passwd != txtPassword.Text)
+                    {
+                        MessageBox.Show("Password incorrect!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login successful!");
+                        if (user.userTypeIdFK == 1)
+                        {
+
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
